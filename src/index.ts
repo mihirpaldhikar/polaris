15c03e6740544f655ba0dba5b74f52f8617ba8a4
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2023-05-10 11:06:03 +0530

    build: Add TailwindCSS.

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Mihir Paldhikar
+ *
+ * Polaris is a proprietary software owned, developed and
+ * maintained by Mihir Paldhikar.
+ *
+ * No part of the software should be distributed or reverse
+ * engineered in any form without the permission of the owner.
+ *
+ * Doing so will result into a legal action without any prior notice.
+ *
+ * All Rights Reserved.
+ */
+
+import "./styles/global.css";
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023 Mihir Paldhikar
+ *
+ * Polaris is a proprietary software owned, developed and
+ * maintained by Mihir Paldhikar.
+ *
+ * No part of the software should be distributed or reverse
+ * engineered in any form without the permission of the owner.
+ *
+ * Doing so will result into a legal action without any prior notice.
+ *
+ * All Rights Reserved.
+ */
+
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
