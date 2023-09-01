649ef0813d49a1ed000dcd458e1ac40f97661fc6
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2023-09-01 22:54:44 +0530

    chore: Modified Next.js config for production build of the website.

--- website/next.config.js
+++ website/next.config.js
@@ -21,6 +21,10 @@
  */
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  swcMinify: true,
+  reactStrictMode: true,
+};
 
 module.exports = nextConfig;
