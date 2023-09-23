407ee9541e81b46908356e302c63e06b66f6a072
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2023-09-23 13:34:01 +0530

    style: Fix Code Formatting.

--- src/components/AnnotationToolbar/AnnotationToolbar.tsx
+++ src/components/AnnotationToolbar/AnnotationToolbar.tsx
@@ -111,7 +111,7 @@ export default function AnnotationToolbar({
                         onColorSelected={(colorHexCode) => {
                           const style: Style[] = [
                             {
-                              name: (inputArgs.initialPayload as Style).name,
+                              name: inputArgs.initialPayload.name,
                               value: colorHexCode,
                               enabled: colorHexCode !== REMOVE_COLOR,
                             },
